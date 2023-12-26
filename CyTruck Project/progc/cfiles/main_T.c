ac58ec28ffdbcf23896355649ecb23c45caec273
lucasjmr <130283171+lucasjmr@users.noreply.github.com>
2023-12-26 23:15:01 +0100

    Update main_T.c - typo fix

--- CyTruck Project/progc/cfiles/main_T.c
+++ CyTruck Project/progc/cfiles/main_T.c
@@ -259,7 +259,7 @@ int main()
     fgets(header, sizeof(header), file); // Skips header line
     if (fscanf(file, "%*[^;];%*[^;];%63[^;];%63[^;];%*[^;];%*[^\n]", town1, town2) != 2)
     {
-        printf("Error while gettings varaibles of csv.\n");
+        printf("Error while gettings variables of csv.\n");
         exit(1);
     }
 
