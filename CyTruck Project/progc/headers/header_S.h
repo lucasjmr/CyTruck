e30fb8b53ebd034f4445758d76af8da71563ccba
lucasjmr <130283171+lucasjmr@users.noreply.github.com>
2023-12-27 16:31:02 +0100

    Update header_S.h

--- CyTruck Project/progc/headers/header_S.h
+++ CyTruck Project/progc/headers/header_S.h
@@ -2,14 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct AVL_S
+struct AVL_S
 {
     int state;
-    unsigned int RouteID; 
+    int RouteID;
+    float sum;
+    float MaxStepDistance;
+    float MinStepDistance;
     unsigned short NumberOfSteps;
-    unsigned short MaxStepDistance;
-    unsigned short MinStepDistance;
-    unsigned int sum;
     struct AVL_S *left;
     struct AVL_S *right;
-}AVL_S;
+};
+
+typedef struct AVL_S *pAVL;
+
+typedef struct TopRouteID
+{
+    int RouteID;
+    float average;
+    float MinStepDistance;
+    float MaxStepDistance;
+} TopRouteID;
+
+int min(int a, int b);
+int max(int a, int b);
+int min3(int a, int b, int c);
+int max3(int a, int b, int c);
+pAVL CreateAVL(int RouteID, float distance);
+pAVL LeftRotate(pAVL root);
+pAVL RightRotate(pAVL root);
+pAVL DoubleLeftRotate(pAVL root);
+pAVL DoubleRightRotate(pAVL root);
+pAVL BalanceAVL(pAVL root);
+pAVL InsertInAVL(pAVL root, int RouteID, float distance, int *h);
+pAVL CreateAVLfromCSV(FILE *file, pAVL root, int *h);
+TopRouteID *GetRouteInfo(pAVL root, TopRouteID *array, int *index);
+TopRouteID *CreateTop50RoutesArray(pAVL root);
+TopRouteID *SortArray(TopRouteID *array);
+void PrintDataInCSV(TopRouteID *array);
