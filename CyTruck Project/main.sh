44b39f479e04343f22615b15db2011baca9ac6c8
lucasjmr <130283171+lucasjmr@users.noreply.github.com>
2023-12-28 11:03:29 +0100

    fixed d1 d2 L options and csv_path

--- CyTruck Project/main.sh
+++ CyTruck Project/main.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
+
+
+
+
+
+
+
+
+# ATTENTION : REFAIRE POUR QUE CA OUVRE LE FICHIER CSV PASSE EN ARGUMENT ET NON TOUJOURS DATA.CSV
+# LE PATH EST STOCKE DANS $(CSV_PATH)
+
+
+
+
 d1_process()
 {
     echo 'Starting d1 process ...'
     start_time=$(date +%s)
 
     # Create csv with top 10 drivers <driver name>;<number of occurence> in temp/datad1.csv
-    tail -n+2 data/data.csv | awk -F';' '{driver_array[$6]++} END {for (i in driver_array) print i ";" driver_array[i]}' | sort -t';' -k2 -n | tail -n10 > temp/datad1.csv
-    # Removes the useless first line containing data infos
+    awk -F ';' 'NR > 1 { key=$6";"$1; if (!(key in driver_array)) { driver_array[key] = 1; array[$6]++; }} END { for (i in array) { print i ";" array[i]; }}' "$CSV_PATH" | sort -t';' -k2 -nr | head -10 > temp/datad1.csv
+    # NR > 1 Removes the useless first line containing data infos
     # 
     # AWK - 
-    #   {driver_array[$6]++} : creates associative array with the 6th field (driver name), and add 1 for each occurence.
-    #   END : Waits for all lines to be processed. 
-    #   {for (i in driver_array) print i ";" driver_array[i]} : formates the output file to be <driver name>;<number of occurence>
-    # 
-    # sorts driver names, most routes at the bottom, less routes at the top of file 
-    # keeps last 10 drivers, prints them in temp/datad1.csv
+    #   key=$6";"$1 creates a key for each line <drivername>;<routeid>
+    #   if the combination of routeid/driver doesnt exist, add it to driver_array. So we can know how much times drivers did a different routeid
+    #   Now we create a new array, wich will add one to every different key for each driver. => number of different routes per driver in associative array
+    #   Next, we send <drivername>;<number_of_occurrences> in the sort command with a pipe.
+    #
+    # Now we can sort driver names, most routes at the top, less routes at the bottom of file 
+    # keeps first 10 drivers, prints them in temp/datad1.csv
 
     echo 'Generating d1 histogram ...'
 
@@ -31,8 +46,8 @@ d2_process()
     start_time=$(date +%s)
 
     # Creates csv with top 10 drivers <driver_name>;<distance> in temp/datad2.csv
-    tail -n+2 data/data.csv | awk -F';' '{distance_array[$6]+=$5} END {for (i in distance_array) print i ";" distance_array[i]}' | sort -t';' -k2 -n | tail -n10 > temp/datad2.csv
-    # Removes the useless first line containing data infos
+    awk -F';' ' NR > 1 {distance_array[$6]+=$5} END {for (i in distance_array) print i ";" distance_array[i]}' "$CSV_PATH" | sort -t';' -k2 -n | tail -n10 > temp/datad2.csv
+    # NR > 1 : Removes the useless first line containing data infos
     # 
     # AWK - 
     #   {distance_array[$6]+=$5} : creates associative array with the 6th field (driver name), and add every distance (5th field) to the associated driver
@@ -55,8 +70,8 @@ Lprocess()
     echo 'Starting L process ...'
     start_time=$(date +%s)
 
-    tail -n+2 data/data.csv | awk -F';' '{route_array[$1]+=$5} END {for (i in route_array) print i ";" route_array[i]}' | sort -t';' -k2 -n | tail -n10 | sort -r > temp/dataL.csv
-    # Removes the useless first line containing data infos
+    awk -F';' ' NR > 1 {route_array[$1]+=$5} END {for (i in route_array) print i ";" route_array[i]}' "$CSV_PATH" | sort -t';' -k2 -n | tail -n10 | sort -r > temp/dataL.csv
+    # NR > 1 : Removes the useless first line containing data infos
     # 
     # AWK - 
     #   {route_array[$1]+=$5} : creates associative array with the 1th field (route id), and add every distance (5th field) to the associated route
@@ -77,13 +92,14 @@ Lprocess()
 
 Tprocess()
 {
-    echo 'Starting T process ...'
-    start_time=$(date +%s)
 
     # 1. Make the executable by calling makefile
     cd progc/makefileFolder
     make t
     cd ../..
+
+    echo 'Starting T process ...'
+    start_time=$(date +%s)
 
     # 2. Run Tprocess
     ./temp/Tprocess
@@ -94,7 +110,29 @@ Tprocess()
 
     end_time=$(date +%s)
     echo "Finished T data file process and plot in $(( end_time - start_time )) seconds."
-    # JE FAIS QUOI ICI POUR FAIRE 'make t' PUIS EXECUTER 'TPROCESS' ?
+}
+
+Sprocess()
+{
+
+    # 1. Make the executable by calling makefile
+    cd progc/makefileFolder
+    make s
+    cd ../..
+
+    echo 'Starting S process ...'
+    start_time=$(date +%s)
+    
+    # 2. Run Tprocess
+    ./temp/Sprocess
+
+    # 3. Create plot
+    echo 'Generating filledcurves plot ...'
+    echo "file = 'temp/dataS.csv'; set terminal png; set output 'images/S.png'; set datafile separator ';'; set title 'Option -s : Distance = f(Route)'; set xlabel 'ROUTE ID'; set ylabel 'DISTANCE (km)'; set xtics rotate by -45 scale 0 font ',8'; plot file using 0:5:3 with filledcurves title 'Distances Max/Min (km)', file using 0:4 smooth mcs with lines title 'Distance average (km)'" | gnuplot
+
+
+    end_time=$(date +%s)
+    echo "Finished S data file process and plot in $(( end_time - start_time )) seconds."
 }
 
 print_help()
@@ -212,6 +250,11 @@ else
     mkdir 'temp'
 fi
 
+if [ $(wc -l < "$CSV_PATH") -lt 51 ] ; then # If the number of lines in given csv is lower than 51 (50 lines + header)
+    echo 'The data file you gave is not valid. Must be more than 50 lines'
+    exit 6
+fi
+
 if [ ! -f 'progc/cfiles/main_T.c' ] || [ ! -f 'progc/headers/header_T.h' ] || [ ! -f 'progc/makefileFolder/makefile' ] || [ ! -f 'progc/cfiles/main_S.c' ] || [ ! -f 'progc/headers/header_S.h' ] ; then # Checks if c, header or makefile file are missing
     echo 'C file and/or header and/or makefile are missing from "progc/"' >&2
     exit 4
@@ -234,7 +277,7 @@ while [ "$#" -gt 0 ] ; do # Process every options
             Tprocess 
             ;;
         "-s") 
-            echo 'Do S' 
+            Sprocess
             ;;
     esac
     shift
